4a166e49145461bdec7b1474989a4189deb33a97
renmurakami <autoeyy@gmail.com>
2022-07-11 16:27:35 +0900

    log in

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -7,7 +7,7 @@ const SignIn: VFC = () => {
 	const toast = useToast();
 
 	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState();
+	const [password, setPassword] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 
 	const signInWithEmail = async () => {
@@ -15,20 +15,51 @@ const SignIn: VFC = () => {
 		const { user, error } = await supabase.auth.signIn({ email, password });
 
 		if (error) {
-			toast.show({ description: error.message, duration: 4000 });
+			toast.show({ description: error.message, duration: 4000, bgColor: "error.500" });
+			setIsLoading(false);
+			return;
 		}
 		setIsLoading(false);
+		console.log(user);
+		toast.show({ description: "sign in!", duration: 4000, bgColor: "success.500" });
+	};
+
+	const signUpWithEmail = async () => {
+		setIsLoading(true);
+		const { user, error } = await supabase.auth.signUp({
+			email,
+			password,
+		});
+
+		if (error) {
+			toast.show({ description: error.message, duration: 4000, bgColor: "error.500" });
+			setIsLoading(false);
+			return;
+		}
+		setIsLoading(false);
+		console.log(user);
+		toast.show({ description: "create account!", duration: 4000, bgColor: "success.500" });
 	};
 
 	return (
 		<SafeAreaView>
 			<Box alignItems="center">
 				<VStack space={4}>
-					<Input placeholder="email" w="75%" />
-					<Input placeholder="password" w="75%" type="password" />
+					<Input placeholder="email" w="75%" autoCapitalize="none" onChangeText={(text) => setEmail(text)} />
+					<Input
+						placeholder="password"
+						w="75%"
+						type="password"
+						autoCapitalize="none"
+						onChangeText={(text) => setPassword(text)}
+					/>
 					<Button colorScheme="green" isLoading={isLoading} onPress={signInWithEmail}>
-						signin
+						sign in
 					</Button>
+					<Button colorScheme="blue" isLoading={isLoading} onPress={signUpWithEmail}>
+						create account
+					</Button>
+					{}
 				</VStack>
 			</Box>
 		</SafeAreaView>
